1d40761b773b877e76df7f1f6d1e95774eb80d04
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2024-06-12 13:22:05 +0530

    Pages Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div>
-      <h1 className='text-red-500'> App </h1>
-    </div>
+   <BrowserRouter>
+   <Routes>
+   <Route path="/" element={<Home />} />
+   <Route path="/about" element={<About />}/>
+   <Route path="/signup" element={<SignUp />}/>
+   <Route path="/signin" element={<SignIn />}/>
+   <Route path="/profile" element={<Profile />}/>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from'react-router-dom';
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
